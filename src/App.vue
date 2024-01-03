4d9cb969036cd1e0b88c798ecf0b029193f9fb02
ALLAN SANTOS <santos.allan90@edu.pucrs.br>
2024-01-03 17:29:54 -0300

    Banner intro created

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import BannerIntro from './components/BannerIntro.vue';
+</script>
+
+<template>
+  <BannerIntro />
+
+  <main>
+    <RouterView />
+  </main>
+
+  <footer>
+    Footer
+  </footer>
+</template>
