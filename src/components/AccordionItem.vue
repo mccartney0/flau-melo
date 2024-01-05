f92b29453e08ea71288af98daf6001d03a24c9d6
ALLAN SANTOS <santos.allan90@edu.pucrs.br>
2024-01-05 17:45:16 -0300

    feat: section created

---
+++ src/components/AccordionItem.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{
+  id: number,
+  label: string,
+  content: string,
+}>()
+
+const openTab = ref(1);
+
+const toggleTab = (tab: number) => {
+  openTab.value = openTab.value === tab ? 0 : tab;
+};
+
+const setTabActive = (tab: number) => {
+  return openTab.value === tab ? 'active-tab' : '';
+}
+</script>
+
+<template>
+  <div class="accordion-item">
+    <label
+      @click="toggleTab(id)"
+      :class="['accordion-label cursor-pointer flex items-center p-2',
+        setTabActive(id),
+      ]">
+      <span class="mr-3">
+        {{ label }}
+      </span>
+
+      <span v-show="openTab === id" class="accordion-icon">
+        <i class="bi bi-chevron-up"></i>
+      </span>
+      <span v-show="openTab !== id" class="accordion-icon">
+        <i class="bi bi-chevron-down"></i>
+      </span>
+    </label>
+
+    <transition name="slide-fade">
+      <div v-show="openTab === id" class="accordion-content p-2 mb-5">
+        {{ content }}
+      </div>
+    </transition>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .accordion-item {
+      width: 600px;
+    .accordion-label {
+      color: var(--vt-c-black-soft);
+      font-size: 1.2rem;
+      font-weight: 600;
+      border-radius: 6px;
+
+      &:hover {
+        background-color: var(--vt-c-white-mute);
+      }
+
+      &.active-tab {
+        background-color: var(--vt-c-white-mute);
+      }
+    }
+    
+    .accordion-content {
+      border-bottom: 1px solid var(--vt-c-white-mute);
+      color: var(--vt-c-indigo);
+      font-size: .95rem;
+      font-weight: 400;
+    }
+
+    .slide-fade-leave-active,
+    .slide-fade-enter-active {
+      transition: opacity 0.5s, transform 0.5s;
+    }
+
+    .slide-fade-enter,
+    .slide-fade-leave-to {
+      opacity: 0;
+      transform: translateY(20px);
+    }
+  }
+</style>
---
+++ src/components/SuccessPaths.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import AccordionItem from './AccordionItem.vue';
+
+  const products = ref([
+    {
+      id: 1,
+      label: 'Life Coaching incluso um emotional intelligence',
+      content: 'Trabalha qualquer meta pessoal que você estabeleça. Caso não tenha uma, temos técnicas eficientes para te ajudar a encontrar. Alguns dos benefícios: quebra de crenças limitantes, criação de crenças fortalecedoras, mudança comportamental significativa, melhora na comunicação, produtividade, gestão das emoções e qualidade de vida.'
+    },
+    {
+      id: 2,
+      label: 'Relatório Emotional Intelligence + Devolutiva',
+      content: 'O Relatório mais utilizado pelas grades empresas e pessoal pós Covid! Uma ferramenta indispensável para as pessoas que desejam entender as próprias competências relacionadas à inteligência emocional.'
+    },
+    {
+      id: 3,
+      label: 'Relatório Comportamental DISC Plus + Devolutiva',
+      content: 'O DISC Plus é uma excelente opção! Com ele você adquire muito autoconhecimento, passa a entender seu potencial em amplitude e seus gaps, tendo aí uma base significativa para iniciar seu processo de desenvolvimento pessoal e/ou profissional.'
+    },
+    {
+      id: 4,
+      label: 'DISC Seleção + Treinamento Coletivo (Colaboradores)',
+      content: 'Recrutamento e seleção mais eficientes! Uma poderosa ferramenta para se ter na hora da contratação ou para  avaliar colaboradores para se saber qual ocomportamento deles quando submetidos à situações específicas. O DISC Seleção destaca as características comportamentais das pessoas como forças, motivações , '
+    },
+    {
+      id: 5,
+      label: 'Treinamento Executivo (a partir de 15 Sessões)',
+      content: 'O treinamento executivo oferece um programa bem completo de desenvolvimento para o profissional que quer Performar. Fazendo uma reprogramação mental personalizada voltada para seus propósitos de ascensão. Reposicionamento profissional, com a ajuda da:  Aromaterapia, Neurociência, Mindfulness e Psicologia Positiva;'
+    },
+  ])
+</script>
+
+<template>
+  <div class="wrapper-success-paths mt-28 gap-20 flex justify-around">
+    <img class="flaudiene-image-2" src="@/assets/flaudiene-corpo-inteiro-2.png" alt="">
+
+    <div class="success-paths-container">
+      <div class="container-title mb-28">
+        Caminhos para o Sucesso
+      </div>
+
+      <AccordionItem
+        v-for="product in products"
+        :key="product.id"
+        :id="product.id"
+        :label="product.label"
+        :content="product.content"
+      />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper-success-paths {
+  .flaudiene-image-2 {
+    height: max-content;
+  }
+
+  .success-paths-container {
+    .container-title {
+      color: var(--vt-c-black-soft);
+      font-size: 3rem;
+      font-weight: 500;
+    }
+  }
+}
+</style>
